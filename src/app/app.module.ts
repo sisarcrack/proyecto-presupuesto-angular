577c7f03d59376c4ac9e4d30bdc28d3553e9c1f8
César Torres <cesarsilent@gmail.com>
2024-07-05 00:37:18 +0200

    aplicacion de presupuesto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
+
+@Component({
+  selector: 'app-ingresar-presupuesto',
+  templateUrl: './ingresar-presupuesto.component.html',
+  styleUrls: ['./ingresar-presupuesto.component.css'],
+})
+export class IngresarPresupuestoComponent {
+  cantidad: number;
+  cantidadIncorrecta: boolean;
+
+  constructor(
+    private _presupuestoService: PresupuestoService,
+    private router: Router
+  ) {
+    this.cantidad = 0;
+    this.cantidadIncorrecta = false;
+  }
+
+  agregar() {
+    if (this.cantidad > 0) {
+      this.cantidadIncorrecta = false;
+      this._presupuestoService.presupuesto = this.cantidad;
+      this._presupuestoService.restante = this.cantidad;
+      this.router.navigate(['/gastos']);
+    } else {
+      this.cantidadIncorrecta = true;
+    }
+  }
+}
